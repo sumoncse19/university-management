0710274782bc71cb3ede457320b81716b9208ea6
sumoncse19 <mdsumoncse19@gmail.com>
2024-07-01 10:13:20 +0600

    custom make validator

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -21,6 +21,22 @@ const userNameSchema = new Schema<IUserName>({
     required: [true, 'First Name is required'],
     trim: true,
     maxlength: [20, 'First Name can not be more than 20 character'],
+    // custom validator
+    // validate: function (value: string) {
+    //   const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1)
+    //   // if (value !== firstNameStr) {
+    //   //   return false
+    //   // }
+    //   // return true
+    //   return firstNameStr === value
+    // },
+    validate: {
+      validator: (value: string) => {
+        const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1)
+        return firstNameStr === value
+      },
+      message: '{VALUE} is not in capitalize formate',
+    },
   },
   middleName: { type: String },
   lastName: { type: String, required: [true, 'Last Name is required'] },
