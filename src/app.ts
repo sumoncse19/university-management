5f337ce6f686753816c7865e433c2de073498f41
sumoncse19 <mdsumoncse19@gmail.com>
2024-07-03 06:38:20 +0600

    update eslint.config.mjs

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ app.use(cors())
 
 // application routes
 app.use('/api/v1/students', StudentRoutes)
+
 const getAController = (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
